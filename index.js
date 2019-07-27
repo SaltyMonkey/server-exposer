a6334a55ac0b1ea6bf06e8c11e8db33014140fef
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2019-07-27 03:35:41 +0300

    update 2

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 module.exports = function ServerExposer(mod) {
-	// Is identical to this:
-	mod.game.initialize("me");
-
 	mod.command.add("se", {
 		$default() {
 			mod.settings.enabled = !mod.settings.enabled;
@@ -14,7 +11,9 @@ module.exports = function ServerExposer(mod) {
 	});	
 
 	mod.hook("S_SPAWN_USER", 15, (event) => {
-		if (!mod.settings.enabled || (!mod.game.me.inDungeon && !mod.settings.showEverywhere)) return;
-		event.guild = `${event.guild} : ${mod.serverList[event.serverId].name}`
+		if (mod.settings.enabled && (mod.game.me.inDungeon || mod.settings.showEverywhere)) {
+			event.guild += ` : ${mod.serverList[event.serverId].name}`
+			return true;
+		}
 	});
 }
